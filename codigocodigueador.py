b6c5ba06d891a858c0fdfa4224b0b081be95c731
RobiPaya <roramos785@gmail.com>
2024-06-19 10:32:48 -0600

    ñ
    ñ

--- codigocodigueador.py
+++ codigocodigueador.py
@@ -2,13 +2,6 @@
 
 print('Roberto doesn´t know how to speak english ')
 
-print('''👋 Hi, I’m tilin
-👀 I’m interested in learn to kidnap people
-🌱 I’m currently learning nothing
-💞️ I’m looking to collaborate on nothing
-📫 How to reach me with a car
-😄 Pronouns: eso/tilin
-⚡ Fun fact: i like to make fun of diego and roberto''')
 
 lista=["VERDADERO","FALSO"]
 print('Roberto doesn´t know how to speak english ')
