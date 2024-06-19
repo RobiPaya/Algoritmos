5e4f245eb1d575050e87c7a09b0af6170eee2b58
MirenAmiano69 <diegosepulvedasalas1@gmail.com>
2024-06-19 10:23:59 -0600

    Ayúdenme no se como meter esta lista aca bien input
    bien input

--- codigocodigueador.py
+++ codigocodigueador.py
@@ -1,1 +1,11 @@
+lista=["VERDADERO","FALSO"]
 print('Roberto doesn´t know how to speak english ')
+a=input("Verdadero/Falso").upper()
+for x in lista:
+    if a in x:
+        if a=="VERDADERO":
+            print("Verda")
+        else:
+            print("falso")
+    else:
+        print("Callate Roberto")
