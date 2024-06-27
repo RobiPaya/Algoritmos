937f304da961edb1de60b926fcb7d9ef316d898e
RobiPaya <roramos785@gmail.com>
2024-06-27 11:37:03 -0600

    actualización#2
    DIEGO DEJA DE COMENTAR

--- manzana.py
+++ manzana.py
@@ -3,7 +3,7 @@ cianuro=float(input("MG : "))
 if peso<50 or peso>120 or cianuro<1:
     print("error")
 else:
-    if cianuro/peso>3:
-        print(cianuro-peso*3)
+    if cianuro/peso>=3:
+        print(cianuro-peso*3+1)
     else:
         print("SEGURO")
