85b3b4538732b793be387dbfd0ccb38d333d2f8c
ElnarcoMAU <mauricioangulo500@gmail.com>
2024-06-27 11:37:26 -0600

    Actualizado
    Roberto y Diego estan bien tontos

--- Turing y la manzana.py
+++ Turing y la manzana.py
@@ -4,7 +4,7 @@ cianuro=float(input("MG : "))
 if peso<50 or peso>120 or cianuro<0:
     print("eror")
 else:
-    if cianuro/peso>3:
-        print(cianuro-peso*3)
+    if cianuro/peso>=3:
+        print(cianuro-peso*3+1)
     else:
         print("SEGURO")
