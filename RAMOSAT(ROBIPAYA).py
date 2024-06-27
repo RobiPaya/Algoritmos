203bbb13e3334395e1ef9d189b6e3cea5a80c88c
RobiPaya <roramos785@gmail.com>
2024-06-26 23:07:50 -0600

    ARREGLE MI PRPIO CODIGO
    YA ESTA BIEN!!!!!!

--- RAMOSAT(ROBIPAYA).py
+++ RAMOSAT(ROBIPAYA).py
@@ -1,17 +1,28 @@
-lista=["GOKUS","VEGETAS","KRILINS","FREEZERS","BULMAS"]
-GOKUS=50
-VEGETAS=40
-KRILINS=1
-FREEZERS=20
-BULMAS=200
-while True:
-    opcion=input("").upper().split()
-    for x in lista:
-        if opcion[1] in x:
-            opcion[1]=x
-    print(opcion)
+entrada='''10 gokus
+1 vegeta
+1 krilin
+90 bulmas
+1 freezer'''.replace("s","").split()
+total=0
+for x in entrada:
+    if x.isnumeric():
+        a=int(x)
+    else:
+        if x=="goku":
+            total+=a*50
+        elif x=="krilin":
+            total+=a
+        elif x=="vegeta":
+            total+=a*40
+        elif x=="freezer":
+            total+=a*20
+        else:
+            total+=a*200
 
-    if opcion=="":
-        break
+print(f"Total de ingresos: {total}\nMulta del SAT: {total*3}\nDeuda: {total*3-total}")
+
+
+
+
 
 
