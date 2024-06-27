0cd3701fe4d952ca15745e445445cf0fff25290d
RobiPaya <roramos785@gmail.com>
2024-06-27 11:26:43 -0600

    Update manzana.py

--- manzana.py
+++ manzana.py
@@ -1,7 +1,7 @@
 peso=int(input("Peso : "))
-cianuro=int(input("MG : "))
+cianuro=float(input("MG : "))
 if peso<50 or peso>120 or cianuro<1:
-    print("eror")
+    print("error")
 else:
     if cianuro/peso>3:
         print(cianuro-peso*3)
