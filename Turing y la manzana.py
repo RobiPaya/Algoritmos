f6bbf281dd08b0a90b2adb0a79e9b0ed04cd51f3
RobiPaya <roramos785@gmail.com>
2024-06-27 12:04:41 -0600

    Delete Turing y la manzana.py

--- Turing y la manzana.py
+++
@@ -1,9 +0,0 @@
-peso=int(input("Peso : "))
-cianuro=float(input("MG : "))
-if peso<50 or peso>120 or cianuro<1:
-    print("error")
-else:
-    if cianuro/peso>=3:
-        print(cianuro-peso*3+1)
-    else:
-        print("SEGURO")
