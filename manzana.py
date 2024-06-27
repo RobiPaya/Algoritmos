944f6aa6fe4f35ebad56da95d4bcb50670e7b01e
RobiPaya <roramos785@gmail.com>
2024-06-27 11:19:49 -0600

    manzana
    lesgo

---
+++ manzana.py
@@ -0,0 +1,9 @@
+peso=int(input("Peso : "))
+cianuro=int(input("MG : "))
+if peso<50 or peso>120 or cianuro<0:
+    print("eror")
+else:
+    if cianuro/peso>3:
+        print(cianuro-peso*3)
+    else:
+        print("SEGURO")
