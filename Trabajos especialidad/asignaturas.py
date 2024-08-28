a7ff28f25af850c2018127b34769aff0e7fe23eb
PedoCaca-sudo <cpedo996@gmail.com>
2024-08-28 09:34:49 -0600

    mJEJOR QUE EL DE ROBRTIO

--- Trabajos especialidad/asignaturas.py
+++ Trabajos especialidad/asignaturas.py
@@ -1,7 +1,13 @@
+import random
 lista=[]
+listya=[]
+a=0
 lamba=int(input("cua´tants materisa' : "))
 for x in range(lamba):
+    calificacaiopn=random.randint(0,10)
     asignaturas=input("ASIGNATURA?:").capitalize()
     lista.append(asignaturas)
+    listya.append(calificacaiopn)
 for x in lista:
-    print(x)
+    print(f"{x} - {listya[a]}")
+    a+=1
--- Trabajos especialidad/numeros ganadires.py
+++ Trabajos especialidad/numeros ganadires.py
@@ -4,5 +4,5 @@ listadegaandarores=[]
 for x in range(gandaira):
     numero=random.randint(1,10000)
     listadegaandarores.append(numero)
-    lies=sorted(listadegaandarores,key=lambda columna:columna[0])
-    print(lies)
+listadegaandarores.sort()
+print(listadegaandarores)
