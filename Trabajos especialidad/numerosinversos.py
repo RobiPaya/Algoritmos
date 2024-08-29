e9a86593ba825512af4f54d221a2d236e7073643
PedoCaca-sudo <cpedo996@gmail.com>
2024-08-29 08:31:54 -0600

    LO hizce ze
    ze

--- Trabajos especialidad/numerosinversos.py
+++ Trabajos especialidad/numerosinversos.py
@@ -1,6 +1,10 @@
 lista=[1,2,3,4,5,6,7,8,9,10]
 lista.reverse()
 organizado=""
+cotna=0
 for x in lista:
-    organizado+=str(x)+","
-print(organizado)
+    if x==1:
+        cotna+=1
+    else:
+        organizado+=str(x)+","
+print(f"{organizado}1")
