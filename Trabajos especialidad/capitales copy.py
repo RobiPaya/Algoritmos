817b0a0046a30d5d7adc991fdd90faad8c84c09d
PedoCaca-sudo <cpedo996@gmail.com>
2024-09-02 12:44:17 -0600

    a

--- Trabajos especialidad/capitales copy.py
+++ Trabajos especialidad/capitales copy.py
@@ -2,11 +2,15 @@ import random
 from estados import *
 sasa=0
 corrrectas=0
+incorrrectas=0
 listadeya=[]
 for x in range(10+sasa):
     posicion=random.randint(1,10)
     listadeya.append(posicion)
-    if posicion in listadeya:
-        sasa+=1
-        continue
-    print("osao")
+    hola=input(f"Capital de {estados1[posicion]}")
+    if hola==estados1[posicion]:
+        corrrectas+=1
+    else:
+        incorrrectas+=1
+print(f"Correctas : {corrrectas}")
+print(f"Incorrectas : {incorrrectas}")
