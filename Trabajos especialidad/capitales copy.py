5a3331f76a07e3e3535b0b6261854ab4ee5d60c5
PedoCaca-sudo <cpedo996@gmail.com>
2024-09-02 12:48:48 -0600

    Sa

--- Trabajos especialidad/capitales copy.py
+++ Trabajos especialidad/capitales copy.py
@@ -3,12 +3,16 @@ from estados import *
 sasa=0
 corrrectas=0
 incorrrectas=0
+hola2=0
 listadeya=[]
 for x in range(10+sasa):
     posicion=random.randint(1,10)
     listadeya.append(posicion)
-    hola=input(f"Capital de {estados1[posicion]}")
-    if hola==estados1[posicion]:
+    hola=input(f"Capital de {estados1[posicion]} : ").title()
+    for x in estadosconcaptal:
+        if x in hola:
+            hola2=x
+    if hola2==estados1[posicion]:
         corrrectas+=1
     else:
         incorrrectas+=1
