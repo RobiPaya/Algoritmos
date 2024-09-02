23e54635f8dbdb5f8250fa7b475ebf0822afbd61
RobiPaya <roramos785@gmail.com>
2024-09-02 13:05:41 -0600

    a

--- Trabajos especialidad/capitales.py
+++ Trabajos especialidad/capitales.py
@@ -3,11 +3,11 @@ import random
 respuestas=[]
 posiciones=[]
 for x in range(10):
-    posicion=random.randint(1,32)
+    posicion=random.randint(0,31)
     posiciones.append(posicion)
     for x in posiciones:
         if x==posicion:
-            posicion=random.randint(1,32)
+            posicion=random.randint(0,31)
     respuesta=input(f"Dame la capital de {estados1[posicion]}: ").title()
     if respuesta==estadosconcaptal[posicion]:
         respuestas.append(respuesta+" Correcta")
