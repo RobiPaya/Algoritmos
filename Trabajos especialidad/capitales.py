90f46a9cdc5f201125f85d7c3634381134c7d1bd
RobiPaya <roramos785@gmail.com>
2024-09-03 08:21:23 -0600

    puch

--- Trabajos especialidad/capitales.py
+++ Trabajos especialidad/capitales.py
@@ -1,17 +1,23 @@
 from estados import *
 import random
-respuestas=[]
-posiciones=[]
-for x in range(10):
+erroress=[]
+aciertos=0
+errores=0
+while True:
     posicion=random.randint(0,31)
-    posiciones.append(posicion)
-    for x in posiciones:
-        if x==posicion:
-            posicion=random.randint(0,31)
     respuesta=input(f"Dame la capital de {estados1[posicion]}: ").title()
     if respuesta==estadosconcaptal[posicion]:
-        respuestas.append(respuesta+" Correcta")
+        estados1.remove(estados1[posicion])
+        estadosconcaptal.remove(estadosconcaptal[posicion])
+        aciertos+=1
+    elif respuesta=="S":
+        print(f"""Total de aciertos: {aciertos}\nTotal de errores:
+              {errores}""")
+        print("Tus errores fueron en")
+        for x in errores:
+            print(x)
+        print("SALIDO")
+        break
     else:
-        respuestas.append(respuesta+" incorrecta")
-for x in respuestas:
-    print(x)
+        erroress.append(estados1[posicion])
+        errores+=1
