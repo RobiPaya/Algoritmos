028d3207e51e07b84385cfb5116186db06e1495e
RobiPaya <roramos785@gmail.com>
2024-09-05 08:31:08 -0600

    d

---
+++ Trabajos especialidad/aorcado.py
@@ -0,0 +1,22 @@
+from palabras import *
+import random
+adivina=""
+palabra=random.choice(palabras)
+tamañopalabra=len(palabra)
+for x in range(tamañopalabra):
+    adivina+="-"
+print(adivina)
+for x in range(6):
+    letra=input("Dame una letra: ").lower()
+    for x in range(len(palabra)):
+        if palabra[x]==letra:
+            adivina.replace(adivina[x],palabra[x])
+    if not letra in palabra:
+        print("esa letra es incorrecta")
+        pass
+if adivina==palabra:
+    print("GANASTE")
+else:
+    print("PERDISTE")
+
+
---
+++ Trabajos especialidad/palabras.py
@@ -0,0 +1,1 @@
+palabras=[]
