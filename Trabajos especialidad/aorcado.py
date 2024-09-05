332c2af93c2b1c356a8f2ef7fdc0e8f6fed3c4e6
RobiPaya <roramos785@gmail.com>
2024-09-05 09:24:49 -0600

    f

--- Trabajos especialidad/aorcado.py
+++ Trabajos especialidad/aorcado.py
@@ -1,19 +1,32 @@
 from palabras import *
 import random
 adivina=""
+oportunidades=0
 palabra=random.choice(palabras)
 tamañopalabra=len(palabra)
 for x in range(tamañopalabra):
     adivina+="-"
-print(adivina)
-for x in range(6):
+adivina1=adivina
+while True:
+    print(adivina)
+    adivina=list(adivina)
     letra=input("Dame una letra: ").lower()
-    for x in range(len(palabra)):
-        if palabra[x]==letra:
-            adivina.replace(adivina[x],palabra[x])
-    if not letra in palabra:
+    for y in range(len(palabra)):
+        if palabra[y]==letra:
+            adivina[y]=letra
+    if letra not in palabra:
         print("esa letra es incorrecta")
-        pass
+        oportunidades+=1
+    else:
+        if oportunidades==0:
+            pass
+        else:
+            oportunidades-=1
+    adivina1=""
+    adivina1=adivina1.join(adivina)
+    adivina=adivina1
+    if oportunidades==6 or adivina==palabra:
+        break
 if adivina==palabra:
     print("GANASTE")
 else:
