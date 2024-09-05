01eac5f831257ade1697895f63d3eb031cbaefd8
PedoCaca-sudo <cpedo996@gmail.com>
2024-09-05 11:20:07 -0600

    bonita ya

--- Trabajos especialidad/aorcado.py
+++ Trabajos especialidad/aorcado.py
@@ -1,4 +1,5 @@
 from palabras import *
+from os import system
 import random
 adivina=""
 oportunidades=0
@@ -8,14 +9,15 @@ for x in range(tamañopalabra):
     adivina+="-"
 adivina1=adivina
 while True:
-    print(adivina)
+    system("cls")
+    print(caca[oportunidades])
+    print(f"\n{adivina}")
     adivina=list(adivina)
     letra=input("Dame una letra: ").lower()
     for y in range(len(palabra)):
         if palabra[y]==letra:
             adivina[y]=letra
     if letra not in palabra or not letra:
-        print("esa letra es incorrecta")
         oportunidades+=1
     adivina1=""
     adivina1=adivina1.join(adivina)
@@ -23,6 +25,10 @@ while True:
     if oportunidades==6 or adivina==palabra:
         break
 if adivina==palabra:
+    system("cls")
     print(f"GANASTE\n{palabra}")
+    print(caca[oportunidades])
 else:
+    system("cls")
+    print(caca[6])
     print(f"PERDISTE\nLa palabra era {palabra}")
--- Trabajos especialidad/palabras.py
+++ Trabajos especialidad/palabras.py
@@ -1,1 +1,44 @@
 palabras=["caca","popo","pipi","pupu","diarrea","chocolate","variable", "funcion", "clase", "objeto", "metodo", "array", "bucle", "condicional", "compilador", "interfaz", "herencia", "polimorfismo", "encapsulamiento", "algoritmo", "depuracion", "sintaxis", "parametro", "argumento", "biblioteca", "framework","compilacion", "ejecucion", "depurador", "excepcion", "recursividad", "optimizacion", "subrutina", "protocolo", "serializacion", "deserializacion", "concurrencia", "asincronia", "hilo", "mutex", "semaforo", "transaccion", "consulta", "base de datos", "modelo", "vista","compilador", "interprete", "bytecode", "token", "parsing", "lexical", "scripting", "macro", "plugin", "middleware", "api", "endpoint", "restful", "soap", "json", "xml", "yaml", "markup", "stylesheet", "responsive"]
+caca=['''+---+
+  |   |
+      |
+      |
+      |
+      |
+''','''+---+
+  |   |
+  ☺   |
+      |
+      |
+      |''',
+      '''+---+
+  |   |
+  ☺   |
+  |   |
+      |
+      |
+      ''','''+---+
+  |   |
+  ☺   |
+ /|   |
+      |
+      |
+''','''+---+
+  |   |
+  ☺   |
+ /|\  |
+      |
+      |
+''','''+---+
+  |   |
+  ☺   |
+ /|\  |
+ /    |
+      |
+''','''+---+
+  |   |
+  ☺   |
+ /|\  |
+ / \  |
+      |
+''']
