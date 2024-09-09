7c6c25a4c7c1977d3c42bfb8845f64548f6b4325
RobiPaya <roramos785@gmail.com>
2024-09-09 11:56:28 -0600

    Create agendatelefonica.py

---
+++ Trabajos especialidad/agendatelefonica.py
@@ -0,0 +1,35 @@
+from os import system
+
+lista=[]
+while True:
+    system("cls")
+    print('''\tAgenda''')
+    print('''A-Agregar\nM-Mostrar\nBO-Borrar\nBU-Buscar\nS-Salir''')
+    opcion=input("DAME LA OPCION QUE QUIERES: ").upper()
+    if opcion=="S":
+        break
+    elif opcion=="A":
+        nombre=input("Nombre: ").title()
+        numero=input("Numero de telefono: ").replace(" ","-")
+        lista.append([nombre,numero])
+    elif opcion=="M":
+        for x in lista:
+            print(f"{x[0]:<10} - {x[1]:>5}")
+    elif opcion=="BO":
+        borrador=input("Cuál vas a matar : ").title()
+        for x in lista:
+            if x[0]==borrador:
+                lista.remove(x)
+                print("borrado")
+        else:
+            print("No encontrado")
+    elif opcion=="BU":
+        buscar=input("Quien BUSCAR: ").title()
+        for x in lista:
+            if buscar in x[0]:
+                print(f"{x[0]:<10} - {x[1]:>5}")
+            else:
+                print("No se encuentra esa persona")
+    else:
+        print("ESA OPCION NO EXISTE")
+    input("Enter para continuar...")         
