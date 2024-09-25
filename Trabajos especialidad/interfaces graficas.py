c13e0dd7bcc6021e60d266c318015c0283ac05ab
RobiPaya <roramos785@gmail.com>
2024-09-25 10:15:58 -0600

    Update interfaces graficas.py

--- Trabajos especialidad/interfaces graficas.py
+++ Trabajos especialidad/interfaces graficas.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 def picale():
-    print("Furra")
+    print("HOLA")
 window=tk.Tk()
 window.title("MY FIRST CACA")
 window.minsize(width=500,height=400)
 titulo=tk.Label(text=("titulo"),font=("Arial",24,"bold"))
 titulo.pack(padx=20,pady=70)
 
-boton=tk.Button(window,text=("Fanta"),command=picale)
+boton=tk.Button(window,text=("Diego"),command=picale)
 boton.pack()
 window.mainloop()
