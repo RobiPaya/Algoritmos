ec99ae3ae0dc040e34ac33e3d5bdcd9e331b8703
RobiPaya <roramos785@gmail.com>
2024-09-26 12:34:04 -0600

    ioji

--- proyecto 2parcial.py/grid.py
+++ proyecto 2parcial.py/grid.py
@@ -1,27 +1,54 @@
 import tkinter as tk
+from tkinter import messagebox
+import random as r
 lista=[]
-listaabecedario=[“a”, “b”, “c”, “d”, “e”, “f”, “g”, “h”, “i”, “j”, “k”, “l”, “m”, “n”, “o”, “p”, “q”, “r”, “s”, “t”, “u”, “v”, “w”, “x”, “y”, “z”, “A”, “B”, “C”, “D”, “E”, “F”, “G”, “H”, “I”, “J”, “K”, “L”, “M”, “N”, “O”, “P”, “Q”, “R”, “S”, “T”, “U”, “V”, “W”, “X”, “Y”, “Z”]
-listanumeros=[“0”, “1”, “2”, “3”, “4”, “5”, “6”, “7”, “8”, “9”]
-listasimbolos=[“!”, “@”, “#”, “$”, “%”, “^”, “&”, “*”, “(”, “)”, “-”, “_”, “=”, “+”, “[”, “]”, “{”, “}”, “;”, “:”, “'”, “"”, “,”, “.”, “/”, “<”, “>”, “?”, “\”, “|”, “~”, “`”]
-def password():
-
-
+listaabecedario=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+listanumeros=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+listasimbolos=['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def archivo():
+    web=entradawebsite.get()
+    user=entradaemail.get()
+    passw=entradapassword.get()
+    if not web or not user or not passw:
+        messagebox.showinfo(title="NADA", message="FALTAN DATOS")
+    else:
+        ok = messagebox.askokcancel(title="Titulo", message="Quieres guardar esta información?")
+        if ok :
+            with open("datos.txt", "a") as archivo:
+                archivo.write(f"{web} | {user} | {passw}\n")
+                entradapassword.delete(0, len(entradapassword.get()))
+                entradaemail.delete(0, len(entradaemail.get()))
+                entradawebsite.delete(0, len(entradawebsite.get()))
+def passwords():
+    rango=r.randint(8,12)
+    entradapassword.delete(0,len(entradapassword.get()))
+    for x in range(rango):
+        opcion=r.randint(1,3)
+        if opcion==1:
+            lista.append(r.choice(listaabecedario))
+        elif opcion==2:
+            lista.append(r.choice(listanumeros))
+        elif opcion==3:
+            lista.append(r.choice(listasimbolos))
+    passwordsss=""
+    passwordsss=passwordsss.join(lista)
+    entradapassword.insert(0,passwordsss)
+    lista.clear()
 window=tk.Tk()
 window.minsize(width=10, height=10)
-
 imagen = tk.PhotoImage(file="imagess.png")
 verimagen = tk.Label(image=imagen).grid(row=1, column=2)
-
 website=tk.Label(text="Website").grid(row=2,column=1)
-entradawebsite=tk.Entry(width=28).grid(row=2, column=2, columnspan=2)
-
+entradawebsite=tk.Entry(width=28)
+entradawebsite.grid(row=2, column=2, columnspan=2)
 email=tk.Label(text="email").grid(row=3,column=1)
-entradaemail=tk.Entry(width=28).grid(row=3, column=2, columnspan=2)
-
+entradaemail=tk.Entry(width=28)
+entradaemail.grid(row=3, column=2, columnspan=2)
 password=tk.Label(text="password").grid(row=4,column=1)
-entradapassword=tk.Entry(width=19).grid(row=4, column=2, columnspan=1)
-generarpassword=tk.Button(text="Generar", width=6).grid(row=4, column=3)
-
-guardar=tk.Button(text="Guardar", width=23).grid(row=5, column=2, columnspan=2)
-
+entradapassword=tk.Entry(width=19)
+entradapassword.grid(row=4, column=2, columnspan=1)
+generarpassword=tk.Button(text="Generar", width=6,command=passwords)
+generarpassword.grid(row=4, column=3)
+guardar=tk.Button(text="Guardar", width=23,command=archivo)
+guardar.grid(row=5, column=2, columnspan=2)
 window.mainloop()
