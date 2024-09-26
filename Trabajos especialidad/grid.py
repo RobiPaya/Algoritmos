2f0e1a6aacbfa56330489e1961c587551b7665c8
PedoCaca-sudo <cpedo996@gmail.com>
2024-09-26 09:51:31 -0600

    toma

---
+++ Trabajos especialidad/grid.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+window=tk.Tk()
+window.minsize(width=10, height=10)
+imagen = tk.PhotoImage(file="Trabajos especialidad/No abrirb.png")
+verimagen = tk.Label(image=imagen).grid(row=1, column=2,)
+website=tk.Label(text="Website").grid(row=2,column=1)
+entradawebsite=tk.Entry().grid(row=2, column=2, columnspan=1)
+email=tk.Label(text="email").grid(row=3,column=1)
+entradaemail=tk.Entry().grid(row=3, column=2, columnspan=1)
+password=tk.Label(text="password").grid(row=4,column=1)
+entradapassword=tk.Entry().grid(row=4, column=2)
+generarpassword=tk.Button(text="Generar").grid(row=4, column=3)
+guardar=tk.Button(text="Guardar").grid(row=5, column=2)
+window.mainloop()
