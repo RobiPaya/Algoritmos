cce44aebb4bbf96d359822118f5083407fb9be4a
RobiPaya <roramos785@gmail.com>
2024-10-08 12:33:11 -0600

    aq

--- proyecto 2parcial.py/grid copy.py
+++ proyecto 2parcial.py/grid copy.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import messagebox
 import random as r
 import json as json
-import pyperclip as py
 #
 lista=[]
 listaabecedario=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -12,37 +11,41 @@ def archivo():
     web=entradawebsite.get()
     user=entradaemail.get()
     passw=entradapassword.get()
-    py.copy(passw)
-    nuevosdatos=({web:{"email":user, "password":passw}})
     #py.copy(passw)
+    nuevosdatos=({web:{"email":user, "password":passw}})
     if not web or not user or not passw:
         messagebox.showinfo(title="NADA", message="FALTAN DATOS")
     else:
         ok = messagebox.askokcancel(title="Titulo", message="Quieres guardar esta información?")
         if ok :
             try:
-                with open("datos.json","r") as data_file:
-                    data=json.load(data_file)
+                with open("datos.json","r") as archivo:
+                    data=json.load(archivo)
                     data.update(nuevosdatos)
-                with open("datos.json","w") as data_file:
-                    json.dump(data,data_file,indent=4)
-                messagebox.showinfo(title="ERROR", message="se creo")
-            except FileNotFoundError:
-                data.update(nuevosdatos)
-                with open("data.json","w"):
-                    json.dump(data, data_file, indent=4) 
-                messagebox.showinfo(title="ERROR", message="se actualizó")
+                with open("datos.json","w") as archivo:
+                    json.dump(data,archivo,indent=4)
+            except:
+                with open("datos.json","w") as archivo:
+                    json.dump(nuevosdatos,archivo,indent=4)
+                messagebox.showinfo(title="Bien", message="Se creo el archivo")
+            finally:
+                entradawebsite.delete(0,len(entradawebsite.get()))
+                entradapassword.delete(0,len(entradapassword.get()))
+                entradaemail.delete(0,len(entradaemail.get()))
 def buscar():
     web=entradawebsite.get()
     if not web:
         messagebox.showinfo(title="NADA", message="FALTAN DATOS")
     else:
-        with open("datos.json","r") as archivo:
-            entradapassword.delete(0,len(entradapassword.get()))
-            entradaemail.delete(0,len(entradaemail.get()))
-            nuevoarchivo=json.load(archivo)
-            entradaemail.insert(0,nuevoarchivo[web]["email"])
-            entradapassword.insert(0,nuevoarchivo[web]["password"])
+        try:
+            with open("datos.json","r") as archivo:
+                entradapassword.delete(0,len(entradapassword.get()))
+                entradaemail.delete(0,len(entradaemail.get()))
+                nuevoarchivo=json.load(archivo)
+                entradaemail.insert(0,nuevoarchivo[web]["email"])
+                entradapassword.insert(0,nuevoarchivo[web]["password"])
+        except:
+            messagebox.showinfo(title="NADA", message="No existe")
 def passwords():
     rango=r.randint(8,12)
     entradapassword.delete(0,len(entradapassword.get()))
