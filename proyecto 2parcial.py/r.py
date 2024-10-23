1431889aa8a8b6e0a155704e05b81536828b7e85
PedoCaca-sudo <cpedo996@gmail.com>
2024-10-23 09:25:40 -0600

    a

--- proyecto 2parcial.py/r.py
+++ proyecto 2parcial.py/r.py
@@ -1,27 +1,88 @@
 import tkinter as tk
-from tkinter import ttk
-class ListboxFrame(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.hscrollbar = ttk.Scrollbar(self, orient=tk.HORIZONTAL)
-        self.vscrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
-        self.listbox = tk.Listbox(
-            self,
-            xscrollcommand=self.hscrollbar.set,
-            yscrollcommand=self.vscrollbar.set
-        )
-        self.hscrollbar.config(command=self.listbox.xview)
-        self.hscrollbar.pack(side=tk.BOTTOM, fill=tk.X)
-        self.vscrollbar.config(command=self.listbox.yview)
-        self.vscrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        self.listbox.pack()
-root = tk.Tk()
-root.title("Lista con barras de desplazamiento")
-root.geometry("400x300")
-listbox_frame = ListboxFrame()
-listbox_frame.listbox.insert(
-    tk.END,
-    *(f"Elemento {i} con un texto muy largo" for i in range(100))
-)
-listbox_frame.pack()
-root.mainloop()
+from tkinter import TclError, ttk
+
+
+def create_input_frame(container):
+
+    frame = ttk.Frame(container)
+
+    # grid layout for the input frame
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=3)
+
+    # Find what
+    ttk.Label(frame, text='Find what:').grid(column=0, row=0, sticky=tk.W)
+    keyword = ttk.Entry(frame, width=30)
+    keyword.focus()
+    keyword.grid(column=1, row=0, sticky=tk.W)
+
+    # Replace with:
+    ttk.Label(frame, text='Replace with:').grid(column=0, row=1, sticky=tk.W)
+    replacement = ttk.Entry(frame, width=30)
+    replacement.grid(column=1, row=1, sticky=tk.W)
+
+    # Match Case checkbox
+    match_case = tk.StringVar()
+    match_case_check = ttk.Checkbutton(
+        frame,
+        text='Match case',
+        variable=match_case,
+        command=lambda: print(match_case.get()))
+    match_case_check.grid(column=0, row=2, sticky=tk.W)
+
+    # Wrap Around checkbox
+    wrap_around = tk.StringVar()
+    wrap_around_check = ttk.Checkbutton(
+        frame,
+        variable=wrap_around,
+        text='Wrap around',
+        command=lambda: print(wrap_around.get()))
+    wrap_around_check.grid(column=0, row=3, sticky=tk.W)
+
+    for widget in frame.winfo_children():
+        widget.grid(padx=5, pady=5)
+
+    return frame
+
+
+def create_button_frame(container):
+    frame = ttk.Frame(container)
+
+    frame.columnconfigure(0, weight=1)
+
+    ttk.Button(frame, text='Find Next').grid(column=0, row=0)
+    ttk.Button(frame, text='Replace').grid(column=0, row=1)
+    ttk.Button(frame, text='Replace All').grid(column=0, row=2)
+    ttk.Button(frame, text='Cancel').grid(column=0, row=3)
+
+    for widget in frame.winfo_children():
+        widget.grid(padx=5, pady=5)
+
+    return frame
+
+
+def create_main_window():
+    root = tk.Tk()
+    root.title('Replace')
+    root.resizable(0, 0)
+    try:
+        # windows only (remove the minimize/maximize button)
+        root.attributes('-toolwindow', True)
+    except TclError:
+        print('Not supported on your platform')
+
+    # layout on the root window
+    root.columnconfigure(0, weight=4)
+    root.columnconfigure(1, weight=1)
+
+    input_frame = create_input_frame(root)
+    input_frame.grid(column=0, row=0)
+
+    button_frame = create_button_frame(root)
+    button_frame.grid(column=1, row=0)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    create_main_window()
