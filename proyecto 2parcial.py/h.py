a688ec89b808974fdd040c9bccbb0f9bf4a61008
RobiPaya <roramos785@gmail.com>
2024-10-23 20:28:31 -0600

    cxfdfdf

--- proyecto 2parcial.py/h.py
+++ proyecto 2parcial.py/h.py
@@ -18,60 +18,95 @@ def reporte():
         cont=0
         cobrototal=0
         diasemanames=filtro.get()
-        with open("reporte.json","r") as archivo:
-            data=js.load(archivo)
-            if diasemanames=="Día":
-                for x in data:
-                    fecha=str(data[x]["Fecha"])
-                    fecha=fecha.replace("/"," ")
-                    fecha=fecha.split()
-                    if fecha[0]==dia:
-                        cont+=1
-                        cobrototal+=data[x]["Cobro"]*1/2
-                respuesta.config(text=f"El total de multas en el dia fueron {cont}\nEl total de cobro fue de ${cobrototal:.2f}")
-            elif diasemanames=="Semana":
-                for x in data:
-                    fecha=str(data[x]["Fecha"])
-                    fecha=fecha.replace("/"," ")
-                    fecha=fecha.split()
-                    fechabien=dt.datetime(int(fecha[2]),int(fecha[1]),int(fecha[0]))
-                    if fechabien.strftime("%A")=="Monday":
-                        diasemana=int(fecha[0])-1
-            elif diasemanames=="Mes":
-                cobrototal=0
-                cont=0
-                for x in data:
-                    fecha=str(data[x]["Fecha"])
-                    hora=str(data[x]["Hora"])
-                    hora=hora.replace(":"," ")
-                    hora=hora.split()
-                    fecha=fecha.replace("/"," ")
-                    fecha=fecha.split()
-                    if fecha[1]==mes:
-                        cont+=1
-                        if int(fecha[0])<int(dia):
-                            cobrototal+=data[x]["Cobro"]
-                        elif int(fecha[0])==int(dia)-1:
-                            if int(hora[0])>horacutual:
-                                cobrototal+=data[x]["Cobro"]
-                            elif int(hora[0])==horacutual:
-                                if int(hora[1])>=minutos:
-                                    cobrototal+=data[x]["Cobro"]
-                                else:
-                                    cobrototal+=data[x]["Cobro"]*1/2
-                        else:
+        try:
+            with open("reporte.json","r") as archivo:
+                data=js.load(archivo)
+                if diasemanames=="Día":
+                    for x in data:
+                        fecha=str(data[x]["Fecha"])
+                        fecha=fecha.replace("/"," ")
+                        fecha=fecha.split()
+                        if fecha[0]==dia:
+                            cont+=1
                             cobrototal+=data[x]["Cobro"]*1/2
-                respuesta.config(text=f"El total de multas en el dia fueron {cont}\nEl total de cobro fue de ${cobrototal:.2f}")
-    frame.grid_forget()
-    frame3.grid(column=0, row=0)
-    filtro=ttk.Combobox(frame3, state="readonly", values=["Día","Semana","Mes","Año"])
-    filtro.grid(column=0, row=0)
-    tk.Button(frame3, text="ver",command=ver).grid(column=0,row=2)
-    eleccion=filtro.get()
-    print(eleccion)
-    tk.Button(frame3, text="Volver", command=verdenuevo).grid(column=0, row=3)
-    respuesta=tk.Label(frame3,text="")
-    respuesta.grid(column=0,row=1)
+                    respuesta.config(text=f"El total de multas en el dia fueron {cont}\nEl total de cobro fue de ${cobrototal:.2f}")
+                elif diasemanames=="Semana":
+                    cobrototal=0
+                    cont=0
+                    diaextra=dt.timedelta(days=0)
+                    fechabien=dt.datetime(int(año),int(mes),int(dia))
+                    if fechabien.strftime("%A")=="Monday":
+                        diaextra=dt.timedelta(days=1)
+                    elif fechabien.strftime("%A")=="Tuesday":
+                        diaextra=dt.timedelta(days=2)
+                    elif fechabien.strftime("%A")=="Wednesday":
+                        diaextra=dt.timedelta(days=3)
+                    elif fechabien.strftime("%A")=="Thursday":
+                        diaextra=dt.timedelta(days=4)
+                    elif fechabien.strftime("%A")=="Friday":
+                        diaextra=dt.timedelta(days=5)
+                    elif fechabien.strftime("%A")=="Saturday":
+                        diaextra=dt.timedelta(days=6)
+                    diainicio=fechabien-diaextra
+                    diafinal=diainicio+dt.timedelta(days=6)
+                    for x in data:
+                        fecha=str(data[x]["Fecha"])
+                        fecha=fecha.replace("/"," ")
+                        fecha=fecha.split()
+                        fechacomparar=dt.datetime(int(fecha[2]),int(fecha[1]),int(fecha[0]))
+                        if fechacomparar>=diainicio and fechacomparar<=diafinal:
+                            cont+=1
+                            if int(fecha[0])<int(dia):
+                                cobrototal+=data[x]["Cobro"]
+                            elif int(fecha[0])==int(dia)-1:
+                                if int(hora[0])>horacutual:
+                                    cobrototal+=data[x]["Cobro"]
+                                elif int(hora[0])==horacutual:
+                                    if int(hora[1])>=minutos:
+                                        cobrototal+=data[x]["Cobro"]
+                                    else:
+                                        cobrototal+=data[x]["Cobro"]*1/2
+                            else:
+                                cobrototal+=data[x]["Cobro"]*1/2
+                    respuesta.config(text=f"El total de multas en el dia fueron {cont}\nEl total de cobro fue de ${cobrototal:.2f}")
+                elif diasemanames=="Mes":
+                    cobrototal=0
+                    cont=0
+                    for x in data:
+                        fecha=str(data[x]["Fecha"])
+                        hora=str(data[x]["Hora"])
+                        hora=hora.replace(":"," ")
+                        hora=hora.split()
+                        fecha=fecha.replace("/"," ")
+                        fecha=fecha.split()
+                        if fecha[1]==mes:
+                            cont+=1
+                            if int(fecha[0])<int(dia):
+                                cobrototal+=data[x]["Cobro"]
+                            elif int(fecha[0])==int(dia)-1:
+                                if int(hora[0])>horacutual:
+                                    cobrototal+=data[x]["Cobro"]
+                                elif int(hora[0])==horacutual:
+                                    if int(hora[1])>=minutos:
+                                        cobrototal+=data[x]["Cobro"]
+                                    else:
+                                        cobrototal+=data[x]["Cobro"]*1/2
+                            else:
+                                cobrototal+=data[x]["Cobro"]*1/2
+                    respuesta.config(text=f"El total de multas en el dia fueron {cont}\nEl total de cobro fue de ${cobrototal:.2f}")
+        except:
+            messagebox.showinfo(title="No hay",message="NO HAY ARCHIVO DE MULTAS")
+        frame.grid_forget()
+        frame3.grid(column=0, row=0)
+        filtro=ttk.Combobox(frame3, state="readonly", values=["Día","Semana","Mes","Año"])
+        filtro.grid(column=0, row=0)
+        tk.Button(frame3, text="ver",command=ver).grid(column=0,row=2)
+        eleccion=filtro.get()
+        print(eleccion)
+        tk.Button(frame3, text="Volver", command=verdenuevo).grid(column=0, row=3)
+        respuesta=tk.Label(frame3,text="")
+        respuesta.grid(column=0,row=1)
+
 
 def multar():
     def enviar():
@@ -114,6 +149,13 @@ def multar():
             except:
                 with open("reporte.json","w") as archivo:
                     js.dump(datos,archivo,indent=4)
+            fechaentrada.delete(0,len(fechaentrada.get()))
+            fechaentrada.insert(0,f"{dia}/{mes}/{año}")
+            nombreentrada.delete(0,len(nombreentrada.get()))
+            matriculaentrada.delete(0,len(matriculaentrada.get()))
+            horaentrada.delete(0,len(horaentrada.get()))
+            tipomultas.delete(0,len(tipomultas.get()))
+            tiposexo.delete(0,len(tiposexo.get()))
     frame.grid_forget()
     frame2.grid(column=0, row=0)
     
