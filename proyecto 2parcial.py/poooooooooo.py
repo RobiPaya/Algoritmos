3666d627cdd0ec52d21515d3a0574513ee33fee2
RobiPaya <roramos785@gmail.com>
2024-10-29 09:43:43 -0600

    Update poooooooooo.py

--- proyecto 2parcial.py/poooooooooo.py
+++ proyecto 2parcial.py/poooooooooo.py
@@ -2,7 +2,13 @@ class figura:
     def __init__(self,lado1,lado2):
         self.lado1=lado1
         self.lado2=lado2
+    
+    def area(self):
+        return self.lado1 * self.lado2
+    
+    def resulta(self):
+        return f"El area del {self.__class__.__name__} es {self.area()}"
 
 #instanciar la clase figura
-cuadro=figura(10,20)
-print(cuadro.lado2)
+cuadro =figura(10,20)
+print(cuadro.resulta())
