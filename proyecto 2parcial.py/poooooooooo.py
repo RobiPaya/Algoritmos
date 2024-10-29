743cd5b748c3ad84842c36826da009499e250b70
PedoCaca-sudo <cpedo996@gmail.com>
2024-10-29 12:03:34 -0600

    Update poooooooooo.py

--- proyecto 2parcial.py/poooooooooo.py
+++ proyecto 2parcial.py/poooooooooo.py
@@ -36,9 +36,25 @@ class cuadrado(figura):
 
 #Definiciones para el tkinter
 def cuadrado_boton():
-    frame.grid_forget()
-    
-    
+    framecirculo.grid_forget() 
+    frametriangula.grid_forget()
+    tk.Label(framecuadrado, text="Lado : ").grid(column=0, row=1)
+    lado=tk.Entry(framecuadrado).grid(column=1, row=1)
+    tk.Button(framecuadrado, text="CALCULAR").grid(column=1, row=2)
+    framecuadrado.grid(column=0, row=1)
+    return lado
+
+def triangulo_boton():
+    framecirculo.grid_forget()
+    framecuadrado.grid_forget()
+    tk.Label(frametriangula, text="Base : ").grid(column=0, row=1)
+    base=tk.Entry(frametriangula).grid(column=1, row=1)
+    tk.Label(frametriangula, text="Altura : ").grid(column=0, row=2)
+    altura=tk.Entry(frametriangula).grid(column=1, row=2)
+    tk.Button(frametriangula, text="CALCULAR").grid(column=1, row=3)
+    frametriangula.grid(column=0, row=1)
+    return base, altura
+
 window=tk.Tk()
 
 frame=tk.Frame(window)
@@ -46,8 +62,20 @@ frame.columnconfigure(0, weight=1)
 frame.grid(column=0, row=0)
 
 tk.Button(frame, text="Cuadrado", command=cuadrado_boton).grid(column=0, row=0)
-tk.Button(frame, text="Triandulo").grid(column=1, row=0)
+tk.Button(frame, text="Triandulo", command=triangulo_boton).grid(column=1, row=0)
 tk.Button(frame, text="Ciruclo").grid(column=2, row=0)
 tk.Button(frame, text="Hexagono").grid(column=3, row=0)
+
+framecuadrado=tk.Frame(window)
+framecuadrado.columnconfigure(0, weight=1)
+framecuadrado.grid(column=0, row=1)
+
+frametriangula=tk.Frame(window)
+frametriangula.columnconfigure(0, weight=1)
+frametriangula.grid(column=0, row=1)
+
+framecirculo=tk.Frame(window)
+framecirculo.columnconfigure(0, weight=1)
+framecirculo.grid(column=0, row=1)
 
 window.mainloop()
