9fcf476ab760890ad349de4411ec075a8ba38465
RobiPaya <roramos785@gmail.com>
2024-11-12 09:24:42 -0600

    Update prueba.py

--- proyecto 2parcial.py/prueba.py
+++ proyecto 2parcial.py/prueba.py
@@ -9,9 +9,13 @@ with open("parcial.csv","r") as archivo:
      for alumno in calificaciones:
           if alumno[5] != "Calificacion":     #[5] es el numero de columna donde esta la calificacion del alumno
                calif.append(float(alumno[5]))
+               if float(alumno[5])==max(calif):
+                    malto=alumno[0]
+               if float(alumno[5])==min(calif):
+                    mameno=alumno[0]
 for x in calif:
      sumacalif+=x
      cont+=1
      if x<60:
           reprobados+=1
-print(sumacalif/cont,reprobados)
+print(sumacalif/cont,reprobados,len(calif)-reprobados,malto,max(calif),mameno,min(calif))
