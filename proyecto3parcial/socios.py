6da613ff63868cba1cc041442c9a3962efef86d9
RobiPaya <roramos785@gmail.com>
2024-11-19 12:28:59 -0600

    d
    gwf

---
+++ proyecto3parcial/socios.py
@@ -0,0 +1,13 @@
+import pandas as pd
+class socios:
+    def registrar(nombre,pago,sexo,edad,entrenador):
+        try:
+            with open("socios.csv") as archivo:
+                pass
+            datos={"nombre":nombre,"pagos":pago,"sexo":sexo,"edad":edad,"entrenador":entrenador}
+            dt=pd.DataFrame(datos)
+            dt.to_csv("socios.csv", mode='a', index=False, header=False)
+        except:
+            datos={"nombre":nombre,"pagos":pago,"sexo":sexo,"edad":edad,"entrenador":entrenador}
+            dt=pd.DataFrame(datos)
+            dt.to_csv("socios.csv", mode='a', index=False)
