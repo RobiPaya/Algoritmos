9c4ca42d616f192e45a121c60486c2ead8610823
PedoCaca-sudo <cpedo996@gmail.com>
2024-11-21 11:19:58 -0600

    tyjud

--- proyecto3parcial/socios.py
+++ proyecto3parcial/socios.py
@@ -1,22 +1,20 @@
 import pandas as pd
 class socios:
-    def registrar(nombre,sexo,edad,entrenador):
+    def registrar(nombre,sexo,edad,entrenador,estado):
         try:
             with open("socios.csv") as archivo:
                 pass
-            datos={"nombre":nombre,"sexo":sexo,"edad":edad,"entrenador":entrenador}
+            datos={"nombre":nombre,"sexo":sexo,"edad":edad,"entrenador":entrenador,"estado":estado}
             dt=pd.DataFrame(datos)
             dt.to_csv("socios.csv", mode='a', index=False, header=False)
         except:
-            datos={"nombre":nombre,"sexo":sexo,"edad":edad,"entrenador":entrenador}
+            datos={"nombre":nombre,"sexo":sexo,"edad":edad,"entrenador":entrenador,"estado":estado}
             dt=pd.DataFrame(datos)
             dt.to_csv("socios.csv", mode='a', index=False)
     def consultar(posicion):
         archivo=pd.read_csv("socios.csv")
         return archivo.iloc[posicion]
-    def editar(posicion):
-        cliente=socios.consultar(posicion)
-        print(cliente[0])
-        
-    
-        
+    def editar(posicion,columna,datoentrada):
+        archivo=pd.read_csv("socios.csv")
+        archivo.loc[[posicion],[columna]]=datoentrada
+        archivo.to_csv("socios.csv",mode="w")   
