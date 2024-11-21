dfd2f34b93ea86d1c128748a1cea32a767b54ff8
RobiPaya <roramos785@gmail.com>
2024-11-21 11:18:28 -0600

    a

--- proyecto3parcial/nuevo.py
+++ proyecto3parcial/nuevo.py
@@ -1,2 +1,22 @@
 from socios import socios
-ola=socios.registrar(nombre=["fatima","negra","diego"],sexo=["m","f","m"],edad=[10,18,70],entrenador=["Fatima","DIEGO","Elvis"])
+from tkinter import Menu
+import tkinter as tk
+from tkinter import PhotoImage
+ola=socios.registrar(nombre=["fatima","negra","diego"],sexo=["m","f","m"],edad=[10,18,70],entrenador=["Fatima","DIEGO","Elvis"],estado=["activo","activo","activo"])
+ventana=tk.Tk()
+ventana.title("Fornite gym")
+ventana.minsize(width=400,height=500)
+barrademenu=Menu
+
+barra_menu = Menu(ventana) 
+menu_archivo = Menu(barra_menu, tearoff=1) 
+barra_menu.add_checkbutton(label="Socios")
+barra_menu.add_checkbutton(label="Asistencias")
+barra_menu.add_checkbutton(label="Reporte")
+barra_menu.add_checkbutton(label="Salir", command=quit)
+ventana.config(menu=barra_menu)
+
+imagen = PhotoImage(file="Logo.png")
+
+
+ventana.mainloop()
